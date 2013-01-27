1c3b76d0aa590df80d597aacdcec72d3654d8c0e
Peter B <zastal@yahoo.com>
2013-01-26 23:02:22 -0600

    createTeam

--- src/main/java/com/pace/sfl/domain/UserProfile.java
+++ src/main/java/com/pace/sfl/domain/UserProfile.java
@@ -7,8 +7,6 @@ import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.layers.repository.mongo.RooMongoEntity;
 import org.springframework.roo.addon.tostring.RooToString;
 
-import java.util.List;
-
 @RooJavaBean
 @RooToString
 @RooMongoEntity
---
+++ src/main/java/com/pace/sfl/web/CreateTeamController.java
@@ -0,0 +1,42 @@
+package com.pace.sfl.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Piotr
+ * Date: 1/26/13
+ * Time: 10:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class CreateTeamController {
+
+    @RequestMapping(value = "/ct", produces = "text/html")
+    public String show()
+    {
+        return "createTeam";
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "saveTeamName", produces = "text/html")
+    public String saveTeamName(@RequestParam("teamName") String teamName,
+                               final HttpServletRequest request)
+    {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName(); //get logged in username
+
+        System.out.println("name: "+name);
+        System.out.println("teamName: "+teamName);
+        return "createTeam";
+    }
+}
